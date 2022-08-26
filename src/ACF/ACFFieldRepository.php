31cdd4a9bf855fa9579f776b9113c841a0b175da
Kim Larsson <kim@humbleguys.se>
2022-08-26 09:53:28 +0200

    Support for creating ACF fields with files

---
+++ src/ACF/ACFFieldRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace HumbleCore\ACF;
+
+class ACFFieldRepository
+{
+    public function loadFieldsFrom(string $path): void
+    {
+        collect(app('files')->files($path))->each(function ($file) {
+            $name = str_replace('.php', '', $file->getFilename());
+
+            $class = str(str_replace(templatePath('app'), '', $file->getPath()))
+                ->replace('/', '\\')
+                ->prepend('\\App')
+                ->append("\\{$name}")
+                ->value();
+
+            $this->register(new $class);
+        });
+    }
+
+    public function register($class): void
+    {
+        register_extended_field_group([
+            'title' => $class::$title,
+            'fields' => $class::fields(),
+            'location' => $class::location(),
+            'position' => 'normal',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'menu_order' => $class::$menuOrder ?? 0,
+        ]);
+    }
+}
---
+++ src/ACF/ACFServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HumbleCore\ACF;
+
+use Illuminate\Support\ServiceProvider;
+
+class ACFServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton('acf', function () {
+        });
+
+        $this->app->singleton('acf.fields', function () {
+            return new ACFFieldRepository;
+        });
+    }
+}
