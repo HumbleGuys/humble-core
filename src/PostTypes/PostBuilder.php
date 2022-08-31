e11f7d65c3fbc260f7af9fe74b6bec7a639b875e
Kim Larsson <kim@humbleguys.se>
2022-08-31 12:47:44 +0200

    Removed some PostBuilder methods

--- src/PostTypes/PostBuilder.php
+++ src/PostTypes/PostBuilder.php
@@ -21,15 +21,11 @@ class PostBuilder
 
     private $acf;
 
-    private $taxQuery;
-
     private $offset = 0;
 
     private $orderBy = 'post_date';
 
     private $order = 'desc';
-
-    private $orderMetaKey;
 
     private $search;
 
@@ -108,57 +104,7 @@ class PostBuilder
         return $this->model;
     }
 
-    public function orderBy(string $field, string $order = 'desc', string $orderBy = 'meta_value'): PostModel
-    {
-        $this->orderMetaKey = $field;
-        $this->order = $order;
-        $this->orderBy = $orderBy;
-
-        return $this->model;
-    }
-
-    public function orderByMetaQuery(array $metas): PostModel
-    {
-        $this->orderBy = [];
-
-        foreach ($metas as $key => $meta) {
-            $this->metaQuery[$key] = [
-                'key' => $key,
-                'type' => $meta['type'],
-            ];
-
-            $this->orderBy[$key] = $meta['order'];
-        }
-
-        return $this->model;
-    }
-
-    public function orderByAsc(string $field): PostModel
-    {
-        return $this->orderBy($field, 'asc');
-    }
-
-    public function orderByDesc(string $field): PostModel
-    {
-        return $this->orderBy($field, 'desc');
-    }
-
-    public function orderByNum(string $field, string $order = 'desc'): PostModel
-    {
-        return $this->orderBy($field, $order, 'meta_value_num');
-    }
-
-    public function orderByNumAsc(string $field): PostModel
-    {
-        return $this->orderByNum($field, 'asc');
-    }
-
-    public function orderByNumDesc(string $field): PostModel
-    {
-        return $this->orderByNum($field, 'desc');
-    }
-
-    public function orderBySortOrder(string $order = 'asc')
+    public function orderByTitle(string $order = 'asc'): PostModel
     {
         $this->orderBy = 'menu_order';
         $this->order = $order;
@@ -166,85 +112,18 @@ class PostBuilder
         return $this->model;
     }
 
-    public function orderByRandom()
+    public function orderBySortOrder(string $order = 'asc'): PostModel
+    {
+        $this->orderBy = 'menu_order';
+        $this->order = $order;
+
+        return $this->model;
+    }
+
+    public function orderByRandom(): PostModel
     {
         $this->orderBy = 'rand';
         $this->order = 'asc';
-
-        return $this->model;
-    }
-
-    public function where($field, $operator = null, $value = null, $type = null, $relation = null)
-    {
-        [$value, $operator] = $this->prepareValueAndOperator(
-            $value,
-            $operator,
-            func_num_args() === 2
-        );
-
-        if (empty($this->metaQuery['metaFilters']['relation'])) {
-            $this->metaQuery['metaFilters']['relation'] = 'AND';
-        }
-
-        if ($relation) {
-            $this->metaQuery['metaFilters']['relation'] = $relation;
-        }
-
-        $this->metaQuery['metaFilters'][] = [
-            'key' => $field,
-            'value' => $value,
-            'type' => $type,
-            'compare' => $operator,
-        ];
-
-        return $this->model;
-    }
-
-    public function whereDate($field, $operator, $value, $relation = null)
-    {
-        return $this->where($field, $operator, $value, 'DATE', $relation);
-    }
-
-    public function orWhere($field, $operator = null, $value = null, $type = null)
-    {
-        return $this->where($field, $operator, $value, $type, 'OR');
-    }
-
-    public function orWhereDate($field, $operator, $value)
-    {
-        return $this->whereDate(...[...func_get_args(), 'OR']);
-    }
-
-    public function prepareValueAndOperator($value, $operator, $useDefault = false)
-    {
-        if ($useDefault) {
-            return [$operator, '='];
-        }
-
-        return [$value, $operator];
-    }
-
-    public function whereTax(?array $items, string $field = 'term_id', string $relation = 'OR'): PostModel
-    {
-        if (! $items) {
-            return $this->model;
-        }
-
-        $query = ['relation ' => $relation];
-
-        foreach ($items as $item) {
-            $query[] = [
-                'taxonomy' => $item['taxonomy'],
-                'field' => $field,
-                'terms' => $item['id'],
-            ];
-        }
-
-        if ($this->taxQuery) {
-            $this->taxQuery = array_merge($query, $this->taxQuery);
-        } else {
-            $this->taxQuery = $query;
-        }
 
         return $this->model;
     }
@@ -366,11 +245,8 @@ class PostBuilder
             'offset' => $this->offset,
             'post__not_in' => $this->excludeIds,
             'post__in' => $postIn,
-            'meta_key' => $this->orderMetaKey,
             'orderby' => $postIn ? 'post__in' : $this->orderBy,
             'order' => $this->order,
-            'tax_query' => $this->taxQuery,
-            'meta_query' => $this->metaQuery,
             's' => $this->search,
             'suppress_filters' => false,
         ]);
