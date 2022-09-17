ac8cf03f4f1f1a3ab5817343c7d112901fdd2e8d
Kim Larsson <kim@humbleguys.se>
2022-09-17 17:29:13 +0200

    Improved active check for archives and taxonomies

--- src/Menu/ActiveChecker.php
+++ src/Menu/ActiveChecker.php
@@ -31,6 +31,10 @@ class ActiveChecker
             return true;
         }
 
+        if ($this->isActiveCustomArchive($menuItem)) {
+            return true;
+        }
+
         return false;
     }
 
@@ -50,5 +54,50 @@ class ActiveChecker
         }
 
         return true;
+    }
+
+    protected function isActiveCustomArchive($menuItem)
+    {
+        if (! $this->currentObject instanceof \WP_Post_Type && empty($this->currentObject->post_type) && ! $this->currentObject instanceof \WP_Term) {
+            return false;
+        }
+
+        $postType = null;
+
+        if ($this->currentObject instanceof \WP_Post_Type) {
+            $postType = $this->currentObject->name;
+        } elseif (! empty($this->currentObject->post_type)) {
+            $postType = $this->currentObject->post_type;
+        } elseif ($this->currentObject instanceof \WP_Term) {
+            return $this->checkCustomArchiveTaxonomy($this->currentObject->taxonomy, $menuItem);
+        }
+
+        return $this->isCustomPostTypeArchive($postType, $menuItem);
+    }
+
+    protected function checkCustomArchiveTaxonomy($taxonomyName, $menuItem)
+    {
+        $taxonomy = app('taxonomies')->getTaxonomy($taxonomyName);
+
+        foreach ($taxonomy->postTypes as $postType) {
+            if ($this->isCustomPostTypeArchive($postType, $menuItem)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function isCustomPostTypeArchive($postType, $menuItem)
+    {
+        if (empty($postType)) {
+            return false;
+        }
+
+        if ($postType === 'post' || $postType === 'page') {
+            return false;
+        }
+
+        return get_post_type_archive_link($postType) === $menuItem->url;
     }
 }
