a1a3b717a437981d7d94bee3638d807271cbdcb6
Kim Larsson <kim@humbleguys.se>
2023-06-06 15:31:40 +0200

    Added consts for command statuses

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -4,6 +4,12 @@ namespace HumbleCore\Console;
 
 class Command
 {
+    public const SUCCESS = 0;
+
+    public const FAILURE = 1;
+
+    public const INVALID = 2;
+
     public function __construct(public $input, public $output)
     {
     }
